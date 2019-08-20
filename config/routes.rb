2cedf61f8dba3f9c39bfef6832b03136e816c5e1
Abhishek Samariya <asamariya@gmail.com>
2019-08-20 15:53:04 +1000

    all working and refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'tasks' => 'tasks#index'
+  resources :tasks
+  # get 'tasks' => 'tasks#index'
 
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
 
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-  patch 'tasks/:id/', to: 'tasks#update'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id/', to: 'tasks#update'
 
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
